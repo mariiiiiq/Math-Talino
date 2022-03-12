9b6819d09952a8726fb44f86a7ca92fbc2c204d2
Maricar Gamay <62501836+mariiiiiq@users.noreply.github.com>
2022-03-12 14:16:23 +0800

    Update upload.php

--- MT/activity/upload.php
+++ MT/activity/upload.php
@@ -2,14 +2,9 @@
 if(!empty($_FILES)){ 
     // Include the database configuration file 
     require 'dbconfig.php'; 
-     
-    // File path configuration 
-    $uploadDir = "uploads/"; 
-    $fileName = basename($_FILES['file']['name']); 
-    $uploadFilePath = $uploadDir.$fileName; 
-     
+   
     // Upload file to server 
-    if(move_uploaded_file($_FILES['file']['tmp_name'], $uploadFilePath)){ 
+    if(is_uploaded_file($_FILES['file']['tmp_name'])){ 
         // Insert file information in the database 
         $sql = "INSERT INTO tblupload (file_name, uploaded_on) VALUES ('".$fileName."', NOW())"; 
         $insert = $conn->query($sql);
