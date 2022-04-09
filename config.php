9174689b07265ee7951780804504958db6b050db
Maricar Gamay <62501836+mariiiiiq@users.noreply.github.com>
2022-04-10 00:41:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SEVER', 'remotemysql.com');
 define('DB_USERNAME', 'ZjQ9CPdSZz');
-define('DB_PASSWORD', 'Um1qQQ8Niq');
+define('DB_PASSWORD', 'CV1vaAvZij');
 define('DB_NAME', 'ZjQ9CPdSZz');
 
 $link = mysqli_connect(DB_SEVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
