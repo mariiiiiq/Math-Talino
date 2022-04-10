0ded57fbbb6e55bc1d97bb01fa72cfe09c20969d
Maricar Gamay <62501836+mariiiiiq@users.noreply.github.com>
2022-04-11 03:00:06 +0800

    Add files via upload

---
+++ lesson2/save.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+require_once "../config.php";
+$data = null;
+if($_POST['type']=="pretest"){
+    if($_POST['act']=="get"){
+        $id= "pre2-".$_SESSION['user'];
+        $sql = "SELECT * FROM tbltest where id = '".$id."' and student='".$_SESSION['user']."'";
+        if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+                    $data = "<b>Score: " . $row['score'] . "/10<br>Date taken: " . $row['datetaken'].'</b>';
+                }
+            }
+            else{
+                $data="";
+            }
+        }
+        else{
+            $data =  "error";
+        }
+        echo $data;
+    }
+    else if($_POST['act']=="save"){
+        $type = "pretest";
+        $id = "pre2-".$_SESSION['user'] ;
+        $sql = "INSERT INTO tbltest VALUES (?, ?, ?, ?, ?)";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "sssss", $id, $_SESSION['user'], $type, $_POST['score'],  $d);
+            $d = date("M d, Y");
+            if(mysqli_stmt_execute($stmt)){
+                echo "</b>Score: " . $_POST['score'];
+                echo "/10<br>Date taken: " . $d .'</b>';
+            }
+            else{
+                echo "Error";
+            }
+        }
+    }
+    
+}
+if($_POST['type']=="posttest"){
+    if($_POST['act']=="get"){
+        $id= "post2-".$_SESSION['user'];
+        $sql = "SELECT * FROM tbltest where id = '".$id."' and student='".$_SESSION['user']."'";
+        if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+                    $data = "<b>Score: " . $row['score'] . "/10<br>Date taken: " . $row['datetaken'].'</b>';
+                }
+            }
+            else{
+                $data="";
+            }
+        }
+        else{
+            $data =  "error";
+        }
+        echo $data;
+    }
+    else if($_POST['act']=="save"){
+        $type = "posttest";
+        $id = "post2-".$_SESSION['user'] ;
+        $sql = "INSERT INTO tbltest VALUES (?, ?, ?, ?, ?)";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "sssss", $id, $_SESSION['user'], $type, $_POST['score'],  $d);
+            $d = date("M d, Y");
+            if(mysqli_stmt_execute($stmt)){
+                echo "</b>Score: " . $_POST['score'];
+                echo "/10<br>Date taken: " . $d .'</b>';
+            }
+            else{
+                echo "Error";
+            }
+        }
+    }
+}
+if($_POST['type']=="activity"){
+    if($_POST['act']=="get"){
+        $id= "act2-".$_SESSION['user'];
+        $sql = "SELECT * FROM tbltest where id = '".$id."' and student='".$_SESSION['user']."'";
+        if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+                    $data = "<b>Score: " . $row['score'] . "/5<br>Date taken: " . $row['datetaken'].'</b>';
+                }
+            }
+            else{
+                $data="";
+            }
+        }
+        else{
+            $data =  "error";
+        }
+        echo $data;
+    }
+    else if($_POST['act']=="save"){
+        $type = "activity";
+        $id = "act2-".$_SESSION['user'] ;
+        $sql = "INSERT INTO tbltest VALUES (?, ?, ?, ?, ?)";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "sssss", $id, $_SESSION['user'], $type, $_POST['score'],  $d);
+            $d = date("M d, Y");
+            if(mysqli_stmt_execute($stmt)){
+                echo "</b>Score: " . $_POST['score'];
+                echo "/5<br>Date taken: " . $d .'</b>';
+            }
+            else{
+                echo "Error";
+            }
+        }
+    }
+    
+}
+?>
