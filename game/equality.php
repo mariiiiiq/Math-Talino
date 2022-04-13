f9f5b102716ff5310b1f44d3aa8a9fe67aa6fc60
Maricar Gamay <62501836+mariiiiiq@users.noreply.github.com>
2022-04-14 07:23:08 +0800

    Add files via upload

---
+++ game/equality.php
@@ -0,0 +1,75 @@
+<?php
+ session_start();
+  if(!isset($_SESSION['user'])){
+    header('Location: ../index.php');
+  }
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://fonts.cdnfonts.com/css/meticula" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="../assets/img/logo-sm.ico" rel="icon">
+    <link href="../assets/css/s.css" rel="stylesheet">
+    <link href="../assets/css/mobile.css" rel="stylesheet">
+    <link href="../assets/css/animations.css" rel="stylesheet">
+    <title>Math-Talino</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-border">
+    <div class="container-fluid">
+      <a class="navbar-brand bold w-50" href="game.php"><img src="../assets/img/logo-sm.png" class="navbar-logo" alt="">Math-Talino</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end mr-5" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="../student.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="../map.php">Lessons</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="../game.php">Games</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            <?php
+            echo $_SESSION['user'];?>
+          </a>
+          <ul class="dropdown-menu drop-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item text-danger" href="../index.php">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <div class="container-fluid">
+      <!--content here-->
+      <a href="../game.php" class="title btnback btn btns">Game list</a>
+      <h2 class="title text-center" style="color:#fee440">Equality</h2>
+      <iframe src="https://phet.colorado.edu/sims/html/fractions-equality/latest/fractions-equality_en.html"
+            class="p-3" alt="" allowfullscreen>
+      </iframe>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+        $(".btnback").mouseenter(function(){
+          $('.btnback').addClass('floating');
+        });
+        $(".btnback").mouseleave(function(){
+          $('.btnback').removeClass('floating');
+        });
+    </script>
+  </body>
+</html>
