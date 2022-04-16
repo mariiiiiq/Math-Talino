5f6102bdeed918d14d7b4605558e2052a4edea01
Maricar Gamay <62501836+mariiiiiq@users.noreply.github.com>
2022-04-16 18:25:49 +0800

    Update config.php

--- Math-talino/config.php
+++ Math-talino/config.php
@@ -2,7 +2,7 @@
 define('DB_SEVER', 'remotemysql.com');
 define('DB_USERNAME', 'ZjQ9CPdSZz');
 define('DB_PASSWORD', 'xA7AW7ldW0');
-define('DB_NAME', 'ZjQ9CPdSZz ');
+define('DB_NAME', 'ZjQ9CPdSZz');
 
 $link = mysqli_connect(DB_SEVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
